64e06e9dee312a3b31d947d8c5d316474e35d9ce
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-08-24 17:13:05 +0100

    update minor markup changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './style.css';
+
+import { refresh, form } from './modules/domElements.js';
+import { addScore, getScores } from './modules/fetchdata.js';
+
+refresh.addEventListener('click', getScores);
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const user = form.name.value;
+  const score = form.score.value;
+  await addScore(user, score);
+  getScores();
+  form.name.value = '';
+  form.score.value = '';
+});
